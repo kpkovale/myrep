e8e306057858ff2b82a9970e584f957feb72d4ff
kpkovale <61504881+kpkovale@users.noreply.github.com>
2020-02-27 23:56:37 +1000

    update 27/02/2020
    * name and surname validation and correction added
    * e-mail validation and lower case added
    * database connection data (user, pwd, host) check added
    * file-path read from parameters added.
    * empty file path user exception added
    * exit on --help request added

--- .github/workflows/user_upload.php
+++ .github/workflows/user_upload.php
@@ -7,10 +7,7 @@ echo "Let's begin!\n";
 $shortopts = "u:p:h:";
 $longopts = ["file:","create_table:","dry_run","help"];
 
-#var_dump($shortopts);
-#var_dump($longopts);
 $options = getopt($shortopts, $longopts);
-#var_dump($options);
 
 const help_responce = <<<EOD
 The following set of commands is specified for current PHP script:
@@ -26,17 +23,59 @@ The following set of commands is specified for current PHP script:
 EOD;
 if (is_null($options["help"])!= 1) echo help_responce;
 
-/*  MAY NEED THIS LATER  */
+#var_dump($options);
+
+if (is_null($options["u"])!= 1) $dbuser = $options["u"];
+if (is_null($options["u"])!= 1) $dbpwd = $options["p"];
+if (is_null($options["u"])!= 1) $dbhost = $options["h"];
+
+if ($dbuser == null or $dbpwd == null or $dbhost == null) {
+    exit("User exception: Database connection parameters required. Please restart the script using correct parameters");
+}
+
+#echo "DBUser: ".$dbuser." DBPassword: ".$dbpwd." DBHost: ".$dbhost."\n";
+/*  --- MAY NEED THIS LATER ---  */
 #using STDOUT on incorrect data responce
 #echo "my message" . PHP_EOL;
 #using STDERR
 #fwrite(STDERR, "hello, world!" . PHP_EOL);
-$f_lines = file('.//test_file.csv', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+/*-----*/
+
+if (is_null($options["file"])!= 1) {
+    $file_path = $options["file"]; // if --file parameter is not empty
+    if (strpos($file_path,'[') == 0 and strpos($file_path,']') == strlen($file_path) - 1) {
+        $file_path = substr($file_path,1,strlen($file_path)-2);
+    }
+}
+
+if ($file_path == null) {
+    exit("User exception: File path or file name is required. Please restart the script using correct parameters");
+}
+
+ #   substr($options["file"],1,strlen($options["file"])-2);
+#echo "test path: ".$file_path."\n";
+#$file_path = './/test_file.csv';
+/*   --- if file parameter is handled on run --> reading file and validating data ---   */
+if ($file_path != null) {
+/*   --- inserting file data into array ---   */
+$f_lines = file($file_path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 for ($i = 0; $i < count($f_lines); $i++)  {
     $file[$i] = explode(";", $f_lines[$i] );
 }
+/*  --- Setting name and surnames to start from Capital letter and lowering other letters ---   */
+for ($i = 0; $i < count($file); $i++) {
+    for ($j = 0; $j < 2; $j++) {
+        $file[$i][$j] = strtoupper(substr($file[$i][$j],0,1)).strtolower(substr($file[$i][$j],1));
+    }
+}
+/*  --- lowering and validating e-mails --- */
+for ($i = 0; $i < count($file); $i++) {
+    $file[$i][2] = strtolower($file[$i][2]);
+    if (filter_var($file[$i][2], FILTER_VALIDATE_EMAIL) != false) $file[$i][3] = true; //e-mail is valid
+    else $file[$i][3] = false; // e-mail is invalid
+}
+#var_dump($file);
+}
 
-#$file = file_get_contents('.//test_file.csv', FILE_USE_INCLUDE_PATH);
-var_dump($file);
 
 ?>
