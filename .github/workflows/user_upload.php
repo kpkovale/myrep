9dac2db6dedf75fec60ccd8cc6fd2eca428f1d31
kpkovale <61504881+kpkovale@users.noreply.github.com>
2020-02-27 22:00:45 +1000

    ver_26.02.20

--- .github/workflows/user_upload.php
+++ .github/workflows/user_upload.php
@@ -1,26 +1,42 @@
-<!doctype html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+
 <?php
 echo "Let's begin!\n";
 
-echo 'param 1 ' . $argv[1] . PHP_EOL;
-echo 'param 2 ' . $argv[2] . PHP_EOL;
-echo 'param 3 ' . $argv[3] . PHP_EOL;
-echo "param count: ".count($argv)."\n";
+#var_dump($argv);
 
-for ($i = 1; $i < count($argv); $i++) {
-    echo "param $i: ".$argv[$i]."\n";
+$shortopts = "u:p:h:";
+$longopts = ["file:","create_table:","dry_run","help"];
+
+#var_dump($shortopts);
+#var_dump($longopts);
+$options = getopt($shortopts, $longopts);
+#var_dump($options);
+
+const help_responce = <<<EOD
+The following set of commands is specified for current PHP script:
+• --file [csv file name] – this is the name of the CSV to be parsed
+• --create_table – this will cause the PostgreSQL users table to be built (and no further action
+    will be taken)
+• --dry_run – this will be used with the --file directive in case we want to run the script but not
+    insert into the DB. All other functions will be executed, but the database won't be altered
+• -u – PostgreSQL username
+• -p – PostgreSQL password
+• -h – PostgreSQL host
+• --help – which will output the above list of directives with details.
+EOD;
+if (is_null($options["help"])!= 1) echo help_responce;
+
+/*  MAY NEED THIS LATER  */
+#using STDOUT on incorrect data responce
+#echo "my message" . PHP_EOL;
+#using STDERR
+#fwrite(STDERR, "hello, world!" . PHP_EOL);
+$f_lines = file('.//test_file.csv', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+for ($i = 0; $i < count($f_lines); $i++)  {
+    $file[$i] = explode(";", $f_lines[$i] );
 }
 
-?>
+#$file = file_get_contents('.//test_file.csv', FILE_USE_INCLUDE_PATH);
+var_dump($file);
 
-</body>
-</html>
+?>
